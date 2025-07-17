08cbfba7eeb8b0f1b77243118800ac0361d3355d
Gang <kingrkddn@gmail.com>
2025-07-17 16:22:01 +0900

    DED
    DED

--- Assets/1. Data Structure/02. Scripts/Fibonacci.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class Fibonacci : MonoBehaviour
-{
-   
-}
--- Assets/1. Data Structure/02. Scripts/MultiDimensionalArray.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class MultiDimensionalArray : MonoBehaviour
-{
-    public int[,] array1 = new int[3, 3];
-}
